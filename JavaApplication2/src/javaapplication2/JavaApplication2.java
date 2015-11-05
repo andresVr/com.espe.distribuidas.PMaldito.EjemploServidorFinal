0c127500c5b734d8347ad4da5382c0a98624ba89
Andrés Rivadeneira <avrz237@gmail.com>
2015-11-05 10:39:29 -0500

    Update JavaApplication2.java

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -18,6 +18,7 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, "Hola");
+           JOptionPane.showMessageDialog(null, "Como estas");
     }
     
 }
